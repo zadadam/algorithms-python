a5cbb04e72582a29a948fd9dba4c2c981d994036
zadadam <Adam.Zadrozny@fuw.edu.pl>
2021-04-07 13:48:39 +0200

    Update bubble.py
    Adding a bad test

--- sort/bubble.py
+++ sort/bubble.py
@@ -27,6 +27,7 @@ class TestBubbleSort(unittest.TestCase):
     def test_sort(self):
         self.assertListEqual(bubble_sort([1, 1]), [1, 1])
         self.assertListEqual(bubble_sort([1, 2]), [1, 2])
+        self.assertListEqual(bubble_sort([1, 2]), [2, 1])
         self.assertListEqual(bubble_sort([2, 1]), [1, 2])
         self.assertListEqual(bubble_sort([6, 3, 9, 1, 43]), [1, 3, 6, 9, 43])
         self.assertListEqual(bubble_sort([14, 46, 43, 27, 57, 41, 45, 21, 70]), [14, 21, 27, 41, 43, 45, 46, 57, 70])
